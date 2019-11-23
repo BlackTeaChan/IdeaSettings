656150e5465a0ffd5151ab96a68004a62fb1ff4e
陈浩 <435661630@qq.com>
2019-11-23 16:46:01 +0800

    IU-2018.3.3 <blackteachan@localhost Create baseRefactoring.xml, ide.general.xml, CodeGlance.xml, project.default.xml, rainbow_brackets.xml, diff.xml, Enum.java, Interface.java, lombok-plugin.xml, other.xml, Default.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, UserAndDate.xml, path.macros.xml, databaseDrivers.xml, github.xml, templates.xml, AnnotationType.java, GrepConsole.xml, Default.xml, Class.java, jdk.table.xml, laf.xml, Mac OS X 10_5_ copy.xml, proxy.settings.xml, vcs.xml, keymap.xml, mybatis.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @author ${USER}
+* 创建日期：${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+*/
+public enum ${NAME} {
+}
